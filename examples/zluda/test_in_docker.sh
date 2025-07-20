8a8d04f804b2f400b8b474f1461c24c397077e14
fabian <fabian.guera@gmail.com>
2025-07-20 19:18:41 +0400

    Update zluda test script (amd docker).

--- examples/zluda/test_in_docker.sh
+++ examples/zluda/test_in_docker.sh
@@ -4,12 +4,13 @@
 echo "Testing ZLUDA integration with configurable backends..."
 
 docker run rocm/pytorch:rocm6.4.1_ubuntu24.04_py3.12_pytorch_release_2.7.1 \
-    bash -c "cd /tmp && \
+    bash -c "apt update && apt install -y npm && npm install -g @bazel/bazelisk && \
+             cd /tmp && \
              git clone https://github.com/gueraf/bazel_torch_thunderkittens.git && \
              cd bazel_torch_thunderkittens && \
              echo '=== Testing ZLUDA-only target ===' && \
-             bazel run //examples/zluda:zluda_only_example && \
+             bazelisk run //examples/zluda:zluda_only_example && \
              echo -e '\n=== Testing configurable target with ZLUDA ===' && \
-             bazel run //examples/zluda:zluda_example --define cuda_backend=zluda && \
+             bazelisk run //examples/zluda:zluda_example --define cuda_backend=zluda && \
              echo -e '\n=== Testing ZLUDA vector addition demo ===' && \
-             bazel run //examples/zluda:zluda_only_vector_add_demo"
+             bazelisk run //examples/zluda:zluda_only_vector_add_demo"
