be26b721d75a61eb846eeb83469d12fdf4b8164f
Fabian Guera <fabian@odyssey.systems>
2025-07-29 14:27:55 +0100

    Add build_test_4090 (half way).

--- examples/thunder_kittens/build_test.cu
+++ examples/thunder_kittens/build_test.cu
@@ -3,6 +3,10 @@
 #include <iostream>
 
 #include "kittens.cuh"
+
+// TODO: Investigate why this fails on 4090
+#ifndef KITTENS_4090
 #include "prototype.cuh"
+#endif
 
 TEST(DummyTest, AlwaysPasses) { std::cout << "Great success" << std::endl; }
